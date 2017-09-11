68d4ad61e07713757a688d175ef104790b71b529
rangertaha <tahasaidi@gmail.com>
2017-09-11 18:31:49 -0400

    init

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='scrapy-prometheus',
+    version='1.0.0',
+    url='https://github.com/rangertaha/scrapy-prometheus',
+    description='Scrapy extenstion to export stats to Prometheus',
+    author='rangertaha',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Framework :: Scrapy',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        'Twisted>=10.0.0',
+        'Scrapy>=0.24.0',
+        'six>=1.5.2',
+    ],
+)
